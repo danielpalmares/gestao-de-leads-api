9285b696fa417595de2a0267266c5dbe6c4fa1bf
Daniel Palmares <danielpalmares100@gmail.com>
2025-10-02 18:18:20 -0300

    feat: implement CORS configuration with allowed origins

---
+++ src/config/cors.ts
@@ -0,0 +1,22 @@
+const allowedOrigins = [
+  'https://danfpl-painel.netlify.app',
+  'https://danfpl-form.netlify.app'
+]
+
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('CORS not allowed'))
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}
+
+export { corsOptions }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express'
-import { router } from './routes/index.ts'
 import cors from 'cors'
+import { router } from './routes/index.ts'
+import { corsOptions } from './config/cors.ts'
 
 const PORT = process.env.PORT || 3000
-
 const app = express()
 
-app.use(cors({ origin: '*' }))
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(router)
 
